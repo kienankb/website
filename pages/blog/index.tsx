bd21ee0b243bfb16d6ca602145d5180e9328aa3e
Kienan Knight-Boehm <kienan@kienankb.com>
2022-04-06 23:36:06 -0700

    use common layout for blog index

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -1,11 +1,17 @@
 import type {NextPage} from 'next';
 import Link from 'next/link';
+import Head from 'next/head';
 
 import {BlogPosts} from '../../data/blog-metadata';
+import Layout from '../../components/layout';
 
 
 const BlogIndex: NextPage = () => {
-  return <>
+  return <Layout>
+    <>
+    <Head>
+      <title>Blog</title>
+    </Head>
     <div>posts!</div>
     {BlogPosts.sort((a, b) => {
       return a.publishDate < b.publishDate ? 1 : -1;
@@ -14,7 +20,8 @@ const BlogIndex: NextPage = () => {
         <Link href={`blog/${post.slug}`}>{post.title}</Link>
       </div>;
     })}
-  </>;
+    </>
+  </Layout>;
 }
 
 export default BlogIndex;
