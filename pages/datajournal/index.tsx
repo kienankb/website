8c52a68f13cc7d714364ad2d7869528b7dfb1be2
Kienan Knight-Boehm <kienan@kienankb.com>
2022-05-13 00:14:24 -0700

    make journal days square, data update

--- pages/datajournal/index.tsx
+++ pages/datajournal/index.tsx
@@ -38,7 +38,7 @@ const HowImDoing: NextPage = () => {
       {days.map(day => <DaySlice key={day.date.format()} day={day} />)}
     </div>
     <header className={styles.headerContainer}>
-      <h1>So...How Have You Been?</h1>
+      <h1>How Have You Been?</h1>
       <h2>a makeshift journal of {days.length || "uhhhh"} days and counting</h2>
     </header>
     <DayGrid days={days} />
