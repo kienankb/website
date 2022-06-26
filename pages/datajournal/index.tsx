fcbc86f2fa741b16a9e7c7974eaa6220aeb74e2b
Kienan Knight-Boehm <kienan@kienankb.com>
2022-06-26 01:25:24 -0700

    data update, title update, add legend

--- pages/datajournal/index.tsx
+++ pages/datajournal/index.tsx
@@ -2,6 +2,7 @@ import type {NextPage} from 'next';
 import {usePapaParse} from 'react-papaparse';
 import moment from 'moment';
 import * as React from 'react';
+import Head from 'next/head';
 
 import {Day, DaySlice, DayGrid} from '../../components/datajournal/DayTile';
 
@@ -34,12 +35,17 @@ const HowImDoing: NextPage = () => {
   React.useEffect(loadDayDataFromCSV, [readRemoteFile]);
 
   return <>
+    <Head>
+      <title>Data Journal</title>
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
     <div className={styles.sliceContainer}>
       {days.map(day => <DaySlice key={day.date.format()} day={day} />)}
     </div>
     <header className={styles.headerContainer}>
       <h1>How Have You Been?</h1>
       <h2>a makeshift journal of {days.length || "uhhhh"} days and counting</h2>
+      <div className={styles.legend}>black = awful, red = bad, orange = okay, green = good, blue = great</div>
     </header>
     <DayGrid days={days} />
   </>;
