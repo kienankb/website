ec963e10d1fc028cb5323ab614d608fbfec6e3ba
Kienan Knight-Boehm <kienan@kienankb.com>
2023-04-25 22:57:52 -0700

    draw line graph instead of days chart

--- pages/datajournal/index.tsx
+++ pages/datajournal/index.tsx
@@ -3,6 +3,7 @@ import {usePapaParse} from 'react-papaparse';
 import moment from 'moment';
 import * as React from 'react';
 import Head from 'next/head';
+import * as D3 from 'd3';
 
 import {Day, DaySlice, DayGrid} from '../../components/datajournal/DayTile';
 
@@ -11,7 +12,7 @@ import styles from '../../components/styles/DataJournal.module.css';
 
 const HowImDoing: NextPage = () => {
   const {readRemoteFile} = usePapaParse();
-  const [days, setDays] = React.useState<Day[]>([]);
+  const [days, setDays] = React.useState<Object[]>([]);
 
   const loadDayDataFromCSV = () => {
     readRemoteFile('/data/days-fixed.csv', {
@@ -26,13 +27,44 @@ const HowImDoing: NextPage = () => {
           return value;
       },
       complete: (results) => {
-        const resultDays = results.data.map(day => day as Day);
-        setDays(resultDays);
+        // const resultDays = results.data.map(day => day as Day);
+        // setDays(resultDays);
+        setDays(results.data);
       },
     });
   };
 
   React.useEffect(loadDayDataFromCSV, [readRemoteFile]);
+
+  const drawLine = () => {
+    const xScale = D3.scaleTime()
+      .domain(D3.extent(days, (day) => day.date.toDate()))
+      .range([0, 1000]);
+    const yScale = D3.scaleLinear()
+      .domain([0, 5])
+      .range([150, 0]);
+    let lineMaker = D3.line()
+      .x((d, i) => {
+        const scaleVal = xScale(d.date);
+        console.debug('x:', d.date.toDate(), scaleVal);
+        return scaleVal;
+      })
+      .y(d => {
+        const scaleVal = yScale(d.rating);
+        console.debug('y:', d.rating, scaleVal);
+        return scaleVal;
+      })
+      .curve(D3.curveBasis);
+    return (
+      <path
+        className="line"
+        d={lineMaker(days)}
+        stroke="url(#linear)"
+        strokeWidth="2px"
+        fill="none"
+      />
+    );
+  };
 
   return <>
     <Head>
@@ -45,10 +77,25 @@ const HowImDoing: NextPage = () => {
     <header className={styles.headerContainer}>
       <h1>How Have You Been?</h1>
       <h2>a makeshift journal of {days.length || "uhhhh"} days and counting</h2>
-      <div className={styles.legend}>black = near-catatonic, red = bad, orange = okay, green = good, blue = great</div>
+      <div className={styles.legend}>darker is a worse day, lighter is a better day; gray is missing data</div>
       <div className={styles.legend}>up above, from left to right, is the same data in a timeline.</div>
     </header>
-    <DayGrid days={days} />
+    <svg
+      viewBox="0 0 1000 150"
+      width={"100%"}
+      height={"150px"}
+      preserveAspectRatio="xMidYMid meet">
+      <defs>
+        <linearGradient id="linear" x1="0%" y1="0%" x2="0%" y2="100%">
+          <stop offset="10%" stop-color="#EAAC8B"/>
+          <stop offset="30%" stop-color="#E56B65"/>
+          <stop offset="50%" stop-color="#B56576"/>
+          <stop offset="70%" stop-color="#6D597A"/>
+          <stop offset="90%" stop-color="#23364c"/>
+        </linearGradient>
+      </defs>
+      {drawLine()}
+    </svg>
   </>;
 }
 
